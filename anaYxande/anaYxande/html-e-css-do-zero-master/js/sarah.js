775f78daff4746812a259ea900f0f02d28acebc5
Ana Villas <anacarolina.vbp@gmail.com>
2024-09-06 11:19:37 -0300

    First Commit

---
+++ anaYxande/anaYxande/html-e-css-do-zero-master/js/sarah.js
@@ -0,0 +1,113 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('myVideo');
+    const playButton = document.getElementById('playButton');
+
+    playButton.addEventListener('click', function() {
+        if (video.paused) {
+            // Inicia a reprodução do vídeo
+            video.play();
+            // Ativa o som do vídeo
+            video.muted = false;
+            // Muda o texto do botão para "Pause"
+            playButton.textContent = '||';
+        } else {
+            // Pausa o vídeo
+            video.pause();
+            // Muda o texto do botão para "Play"
+            playButton.textContent = '▶';
+        }
+    });
+});
+
+
+// TEXTO
+
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('myVideo-virus');
+    const playButton = document.getElementById('playButton-virus');
+
+    playButton.addEventListener('click', function() {
+        if (video.paused) {
+            // Inicia a reprodução do vídeo
+            video.play();
+            // Ativa o som do vídeo
+            video.muted = false;
+            // Muda o texto do botão para "Pause"
+            playButton.textContent = '||';
+        } else {
+            // Pausa o vídeo
+            video.pause();
+            // Muda o texto do botão para "Play"
+            playButton.textContent = '▶';
+        }
+    });
+});
+
+
+
+// VIDEO CLEOPATRA
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('myVideo-cleo');
+    const playButton = document.getElementById('playButton-cleo');
+
+    playButton.addEventListener('click', function() {
+        if (video.paused) {
+            // Inicia a reprodução do vídeo
+            video.play();
+            // Ativa o som do vídeo
+            video.muted = false;
+            // Muda o texto do botão para "Pause"
+            playButton.textContent = '||';
+        } else {
+            // Pausa o vídeo
+            video.pause();
+            // Muda o texto do botão para "Play"
+            playButton.textContent = '▶';
+        }
+    });
+});
+
+
+
+// VIDEO RAVENCROFT
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('myVideo-raven');
+    const playButton = document.getElementById('playButton-raven');
+
+    playButton.addEventListener('click', function() {
+        if (video.paused) {
+            // Inicia a reprodução do vídeo
+            video.play();
+            // Ativa o som do vídeo
+            video.muted = false;
+            // Muda o texto do botão para "Pause"
+            playButton.textContent = '||';
+        } else {
+            // Pausa o vídeo
+            video.pause();
+            // Muda o texto do botão para "Play"
+            playButton.textContent = '▶';
+        }
+    });
+});
+// VIDEO PERICLES
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('myVideo-pericles');
+    const playButton = document.getElementById('playButton-pericles');
+
+    playButton.addEventListener('click', function() {
+        if (video.paused) {
+            // Inicia a reprodução do vídeo
+            video.play();
+            // Ativa o som do vídeo
+            video.muted = false;
+            // Muda o texto do botão para "Pause"
+            playButton.textContent = '||';
+        } else {
+            // Pausa o vídeo
+            video.pause();
+            // Muda o texto do botão para "Play"
+            playButton.textContent = '▶';
+        }
+    });
+});
